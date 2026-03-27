a64c3d70225dd4849caf62b772d0e7fb086fb8e5
franklindesign <manghi@gmail.com>
2026-03-26 18:40:51 -0700

    removed sticky nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="sticky top-0 z-50" role="header">
+        <div>
           <IchibanHeader />
           <SearchComponent />
         </div>
